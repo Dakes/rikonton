7fe5cd67443329b7e7f16dcf44c6f62ba9503a77
Dakes <daniel.ostertag@gmx.net>
2019-07-11 22:05:17 +0200

    destroy road at storage pos, miner pick up again (temporary)

--- structures.js
+++ structures.js
@@ -10,14 +10,19 @@ module.exports =
         try{spawn.room.memory.storage_counter++;}      // boolean
         catch(e){spawn.room.memory.storage_counter = 0;}
 
+        // build storage one left of spawn
         if(!spawn.room.StructureStorage && spawn.room.memory.storage_counter > 1000)
         {
             console.log("trying to build storage");
-            // build storage one left of spawn
+
             let x_coord = spawn.pos.x - 1;
             let y_coord = spawn.pos.y;
 
-            // TODO: delete street at this position
+            let road = spawn.room.lookForAt(STRUCTURE_ROAD, x_coord, y_coord);
+            if(road[0] && road[0].structureType === STRUCTURE_ROAD)
+            {
+                road.destroy();
+            }
 
             spawn.room.createConstructionSite(x_coord, y_coord, STRUCTURE_STORAGE);
 
